b3bbee38160fb697ec7208be21ed5743ba76503b
Robbe Schepens <robbe.schepens.y2870@student.hogent.be>
2018-02-22 11:45:44 +0100

    Create test project

---
+++ BreakOutBoxG22.Tests/Models/SessieTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+
+namespace BreakOutBoxG22.Tests.Models
+{
+    class SessieTest
+    {
+    }
+}
