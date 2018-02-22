cc10a82e157d9dc50a7ff1ee3a4bb156b97290c7
Daan Dedecker <daan.dedecker.y0420@student.hogent.be>
2018-02-22 21:54:07 +0100

    Klasse Klas + Leerkracht aanvullen

--- BreakOutBox/Models/Domain/Klas.cs
+++ BreakOutBox/Models/Domain/Klas.cs
@@ -8,6 +8,15 @@ namespace BreakOutBox.Models.Domain
     {
 
         public int KlasId { get; set; }
-        public int UniekeLeerlingen { get; set; } // de leerlingen die in een klas zitten
+        public ICollection<Leerling> LeerlingenInKlas { get; set; } // de leerlingen die in een klas zitten
+        public ICollection<Leerkracht> LeerkrachtenInKlas { get; set; }
+
+
+        public Klas(ICollection<Leerling>  leerlingenInKlas, ICollection<Leerkracht> leerkrachtenInKlas)
+        {
+            this.LeerlingenInKlas = leerlingenInKlas;
+            this.LeerkrachtenInKlas = leerkrachtenInKlas;
+
+        }
     }
 }
---
+++ BreakOutBox/Models/Domain/Leerkracht.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreakOutBox.Models.Domain
+{
+    public class Leerkracht
+    {
+
+        public int LeerkrachtId { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public Klas klas { get; set; }
+
+
+        public Leerkracht(string voornaam, string achternaam, Klas klas)
+        {
+            this.Voornaam = voornaam;
+            this.Achternaam = achternaam;
+            this.klas = klas;
+        }
+
+    }
+}
