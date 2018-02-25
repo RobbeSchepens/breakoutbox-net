76d070f169796ec9e7f9e345ec4c8a4bd4e01445
Daan Dedecker <daan.dedecker.y0420@student.hogent.be>
2018-02-25 16:44:00 +0100

    GetSessieByCode in sessieRepo

---
+++ BreakOutBox/Data/Repositories/SessieRepository.cs
@@ -0,0 +1,71 @@
+﻿using BreakOutBox.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreakOutBox.Data.Repositories
+{
+    public class SessieRepository : ISessieRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Sessie> _sessies;
+
+        public SessieRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _sessies = context.Sessies;
+        }
+
+       
+
+        public IEnumerable<Sessie> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Sessie GetSessieByCode(string sessieCode)
+        {
+            return _sessies.Include(s => s.Groepen)
+                .ThenInclude(grp => grp.Leden)
+                .Include(s => s.Groepen)
+                    .ThenInclude(grp => grp.Paden)
+                    .ThenInclude(pad => pad.Opdrachten)
+                    .ThenInclude(opdr => opdr.Oefeningen)
+                    .ThenInclude(oef => oef.Opgave)
+                    .ThenInclude(Opgave => Opgave.Vragen)
+                .Include(s => s.Groepen)
+                    .ThenInclude(grp => grp.Paden)
+                    .ThenInclude(pad => pad.Opdrachten)
+                    .ThenInclude(opdr => opdr.Oefeningen)
+                    .ThenInclude(oef => oef.Feedback)
+                .Include(s => s.Groepen)
+                    .ThenInclude(grp => grp.Paden)
+                    .ThenInclude(pad => pad.Opdrachten)
+                    .ThenInclude(opdr => opdr.Oefeningen)
+                    .ThenInclude(oef => oef.Groepsbewerkingen)
+                    
+                .Include(s => s.Groepen)
+                    .ThenInclude(grp => grp.Paden)
+                    .ThenInclude(pad => pad.Acties)
+                .FirstOrDefault(s => s.Code == sessieCode);
+        }
+
+        public void Add(Sessie sessie)
+        {
+            _sessies.Add(sessie);
+        }
+
+        public void Delete(Sessie sessie)
+        {
+            _sessies.Remove(sessie);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ BreakOutBox/Models/Domain/ISessieRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreakOutBox.Models.Domain
+{
+    public interface ISessieRepository
+    {
+
+        IEnumerable<Sessie> GetAll();
+        Sessie GetSessieByCode(string sessieCode);
+
+
+        void Add(Sessie sessie);
+        void Delete(Sessie sessie);
+        void SaveChanges();
+
+    }
+}
