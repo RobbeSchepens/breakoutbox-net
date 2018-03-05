0483a62ed468f83f12623e0f0b52cb8698b5ca77
Robbe Schepens <robbe.schepens.y2870@student.hogent.be>
2018-03-05 08:57:50 +0100

    Add content to oef and opdracht in initializer

--- BreakOutBox/Data/BreakOutBoxDataInitializer.cs
+++ BreakOutBox/Data/BreakOutBoxDataInitializer.cs
@@ -46,8 +46,6 @@ namespace BreakOutBox.Data
                 };
                 #endregion
 
-
-
                 #region BoxAanmaken
                 var toegangscodes = new List<Toegangscode> { // deze code geeft aan wat je volgende oefening is (bv. de code die je in een balon terugvind)
                     new Toegangscode(0),
@@ -60,10 +58,6 @@ namespace BreakOutBox.Data
                     new Toegangscode(0),
                 };
 
-
-
-              
-
                 var acties = new List<Actie> { // de mogelijke acties dat je moet ondernemen (in een balon prikken, telefoonboek opendoen)
                     new Actie(""),
                     new Actie(""),
@@ -75,112 +69,99 @@ namespace BreakOutBox.Data
                     new Actie(""),
                 };
 
-
                 var oefeningen = new List<Oefening> { // dit zijn de oefeningen die in de sessie gebruikt worden
-                    new Oefening("Oefening1", "", 0, ""),
-                    new Oefening("Oefening2", "", 0, ""),
-                    new Oefening("Oefening3", "", 0, ""),
-                    new Oefening("Oefening4", "", 0, ""),
-                    new Oefening("Oefening5", "", 0, ""),
-                    new Oefening("Oefening6", "", 0, ""),
-                    new Oefening("Oefening7", "", 0, ""),
-                    new Oefening("Oefening8", "", 0, "")
+                    new Oefening("Oefening1", "oefening1.pdf", 11.1, "Tel 5 erbij op"),
+                    new Oefening("Oefening2", "oefening2.pdf", 22.2, "Trek 5 ervan af"),
+                    new Oefening("Oefening3", "oefening3.pdf", 33.3, "Doe maal 5"),
+                    new Oefening("Oefening4", "oefening4.pdf", 44.4, "Tel 5 erbij op"),
+                    new Oefening("Oefening5", "oefening5.pdf", 55.5, "Tel 5 erbij op"),
+                    new Oefening("Oefening6", "oefening6.pdf", 66.6, "Tel 5 erbij op"),
+                    new Oefening("Oefening7", "oefening7.pdf", 77.7, "Tel 5 erbij op"),
+                    new Oefening("Oefening8", "oefening8.pdf", 88.8, "Tel 5 erbij op")
                 };
-               
 
-
-                Box box = new Box(acties,oefeningen,toegangscodes);
-
-
+                Box box = new Box(acties, oefeningen, toegangscodes);
                 #endregion
 
+                #region OpdrachtenMaken
+                //Oefening testest = box.Oefeningen.ToList()[0];
+                var act = box.Acties.ToList();
+                var oef = box.Oefeningen.ToList();
+                var toe = box.Toegangscodes.ToList();
 
-
-
-
-
-                #region OpdrachtenMaken  
-
-                
-
-                Oefening testest = box.Oefeningen.ToList()[0];
                 var opdrachtenGroep1 = new List<Opdracht>{ // lijst met alle opdrachten van groep1 (1,2, 3, 4, 5, 6, 7, 8)             
-                    new Opdracht(2,null,box.Oefeningen.ToList()[0],null),
-                    new Opdracht(2,null,box.Oefeningen.ToList()[1],null),
-                    new Opdracht(3,null,box.Oefeningen.ToList()[2],null),
-                    new Opdracht(4,null,box.Oefeningen.ToList()[3],null),
-                    new Opdracht(5,null,box.Oefeningen.ToList()[4],null),
-                    new Opdracht(6,null,box.Oefeningen.ToList()[5],null),
-                    new Opdracht(7,null,box.Oefeningen.ToList()[6],null),
-                    new Opdracht(8,null,box.Oefeningen.ToList()[7],null)
+                    new Opdracht(1,act[0],oef[0],toe[0]),
+                    new Opdracht(2,act[1],oef[1],toe[1]),
+                    new Opdracht(3,act[2],oef[2],toe[2]),
+                    new Opdracht(4,act[3],oef[3],toe[3]),
+                    new Opdracht(5,act[4],oef[4],toe[4]),
+                    new Opdracht(6,act[5],oef[5],toe[5]),
+                    new Opdracht(7,act[6],oef[6],toe[6]),
+                    new Opdracht(8,act[7],oef[7],toe[7])
                 };
-                var opdrachtenGroep2 = new List<Opdracht>{ // lijst met alle opdrachten van groep2 (7, 6, 5, 4, 3, 2, 1, 8)
-                    new Opdracht(9,null,box.Oefeningen.ToList()[6],null),
-                    new Opdracht(10,null,box.Oefeningen.ToList()[5],null),
-                    new Opdracht(11,null,box.Oefeningen.ToList()[4],null),
-                    new Opdracht(12,null,box.Oefeningen.ToList()[3],null),
-                    new Opdracht(13,null,box.Oefeningen.ToList()[2],null),
-                    new Opdracht(14,null,box.Oefeningen.ToList()[1],null),
-                    new Opdracht(15,null,box.Oefeningen.ToList()[0],null),
-                    new Opdracht(16,null,box.Oefeningen.ToList()[7],null)
-                };
-                var opdrachtenGroep3 = new List<Opdracht>{ // lijst met alle opdrachten van groep3 (3, 5, 7, 1, 2, 4, 6, 8)
-                    new Opdracht(17,null,box.Oefeningen.ToList()[2],null),
-                    new Opdracht(18,null,box.Oefeningen.ToList()[4],null),
-                    new Opdracht(19,null,box.Oefeningen.ToList()[6],null),
-                    new Opdracht(20,null,box.Oefeningen.ToList()[0],null),
-                    new Opdracht(21,null,box.Oefeningen.ToList()[1],null),
-                    new Opdracht(22,null,box.Oefeningen.ToList()[3],null),
-                    new Opdracht(23,null,box.Oefeningen.ToList()[5],null),
-                    new Opdracht(24,null,box.Oefeningen.ToList()[7],null)
-                };
-                var opdrachtenGroep4 = new List<Opdracht>{ // lijst met alle opdrachten van groep4 ( 6, 2, 5, 1, 4, 7, 3, 8 )
-                    new Opdracht(25,null,box.Oefeningen.ToList()[5],null),
-                    new Opdracht(26,null,box.Oefeningen.ToList()[1],null),
-                    new Opdracht(27,null,box.Oefeningen.ToList()[4],null),
-                    new Opdracht(28,null,box.Oefeningen.ToList()[0],null),
-                    new Opdracht(29,null,box.Oefeningen.ToList()[3],null),
-                    new Opdracht(30,null,box.Oefeningen.ToList()[6],null),
-                    new Opdracht(31,null,box.Oefeningen.ToList()[2],null),
-                    new Opdracht(32,null,box.Oefeningen.ToList()[7],null)
-                };
-                var opdrachtenGroep5 = new List<Opdracht>{ // lijst met alle opdrachten van groep5 (4, 3, 6, 2, 7, 5, 1, 8)
-                    new Opdracht(33,null,box.Oefeningen.ToList()[3],null),
-                    new Opdracht(34,null,box.Oefeningen.ToList()[2],null),
-                    new Opdracht(35,null,box.Oefeningen.ToList()[5],null),
-                    new Opdracht(36,null,box.Oefeningen.ToList()[1],null),
-                    new Opdracht(37,null,box.Oefeningen.ToList()[6],null),
-                    new Opdracht(38,null,box.Oefeningen.ToList()[4],null),
-                    new Opdracht(39,null,box.Oefeningen.ToList()[0],null),
-                    new Opdracht(40,null,box.Oefeningen.ToList()[7],null)
-                };
-
+                //var opdrachtenGroep2 = new List<Opdracht>{ // lijst met alle opdrachten van groep2 (7, 6, 5, 4, 3, 2, 1, 8)
+                //    new Opdracht(9,null,box.Oefeningen.ToList()[6],null),
+                //    new Opdracht(10,null,box.Oefeningen.ToList()[5],null),
+                //    new Opdracht(11,null,box.Oefeningen.ToList()[4],null),
+                //    new Opdracht(12,null,box.Oefeningen.ToList()[3],null),
+                //    new Opdracht(13,null,box.Oefeningen.ToList()[2],null),
+                //    new Opdracht(14,null,box.Oefeningen.ToList()[1],null),
+                //    new Opdracht(15,null,box.Oefeningen.ToList()[0],null),
+                //    new Opdracht(16,null,box.Oefeningen.ToList()[7],null)
+                //};
+                //var opdrachtenGroep3 = new List<Opdracht>{ // lijst met alle opdrachten van groep3 (3, 5, 7, 1, 2, 4, 6, 8)
+                //    new Opdracht(17,null,box.Oefeningen.ToList()[2],null),
+                //    new Opdracht(18,null,box.Oefeningen.ToList()[4],null),
+                //    new Opdracht(19,null,box.Oefeningen.ToList()[6],null),
+                //    new Opdracht(20,null,box.Oefeningen.ToList()[0],null),
+                //    new Opdracht(21,null,box.Oefeningen.ToList()[1],null),
+                //    new Opdracht(22,null,box.Oefeningen.ToList()[3],null),
+                //    new Opdracht(23,null,box.Oefeningen.ToList()[5],null),
+                //    new Opdracht(24,null,box.Oefeningen.ToList()[7],null)
+                //};
+                //var opdrachtenGroep4 = new List<Opdracht>{ // lijst met alle opdrachten van groep4 ( 6, 2, 5, 1, 4, 7, 3, 8 )
+                //    new Opdracht(25,null,box.Oefeningen.ToList()[5],null),
+                //    new Opdracht(26,null,box.Oefeningen.ToList()[1],null),
+                //    new Opdracht(27,null,box.Oefeningen.ToList()[4],null),
+                //    new Opdracht(28,null,box.Oefeningen.ToList()[0],null),
+                //    new Opdracht(29,null,box.Oefeningen.ToList()[3],null),
+                //    new Opdracht(30,null,box.Oefeningen.ToList()[6],null),
+                //    new Opdracht(31,null,box.Oefeningen.ToList()[2],null),
+                //    new Opdracht(32,null,box.Oefeningen.ToList()[7],null)
+                //};
+                //var opdrachtenGroep5 = new List<Opdracht>{ // lijst met alle opdrachten van groep5 (4, 3, 6, 2, 7, 5, 1, 8)
+                //    new Opdracht(33,null,box.Oefeningen.ToList()[3],null),
+                //    new Opdracht(34,null,box.Oefeningen.ToList()[2],null),
+                //    new Opdracht(35,null,box.Oefeningen.ToList()[5],null),
+                //    new Opdracht(36,null,box.Oefeningen.ToList()[1],null),
+                //    new Opdracht(37,null,box.Oefeningen.ToList()[6],null),
+                //    new Opdracht(38,null,box.Oefeningen.ToList()[4],null),
+                //    new Opdracht(39,null,box.Oefeningen.ToList()[0],null),
+                //    new Opdracht(40,null,box.Oefeningen.ToList()[7],null)
+                //};
                 #endregion
+
                 #region PadenAanmaken
                 var paden = new List<Pad> { // elk pad heeft zijn eigen volgorde van vragen (region: LijstenMetOpdrachtenPerGroep)
                     new Pad(opdrachtenGroep1),
-                    new Pad(opdrachtenGroep2),
-                    new Pad(opdrachtenGroep3),
-                    new Pad(opdrachtenGroep4),
-                    new Pad(opdrachtenGroep5)
+                    //new Pad(opdrachtenGroep2),
+                    //new Pad(opdrachtenGroep3),
+                    //new Pad(opdrachtenGroep4),
+                    //new Pad(opdrachtenGroep5)
                 };
                 #endregion
 
                 #region LeerlingenInGroepenSteken
-
                 var groepen = new List<Groep> {
                   new Groep(paden[0], leerlingen.GetRange(0, 4)),
-                  new Groep(paden[1], leerlingen.GetRange(4, 4)),
-                  new Groep(paden[2], leerlingen.GetRange(8, 4)),
-                  new Groep(paden[3], leerlingen.GetRange(12, 4)),
-                  new Groep(paden[4], leerlingen.GetRange(16, 4))
+                  //new Groep(paden[1], leerlingen.GetRange(4, 4)),
+                  //new Groep(paden[2], leerlingen.GetRange(8, 4)),
+                  //new Groep(paden[3], leerlingen.GetRange(12, 4)),
+                  //new Groep(paden[4], leerlingen.GetRange(16, 4))
                 };
                 #endregion
-
-
+                
                 #region KlasEnLeerkracht
-
-
                 Leerkracht lk = new Leerkracht("Tom", "Deveylder");
                 var k = new Klas();
                 k.Leerlingen = leerlingen;
@@ -191,17 +172,10 @@ namespace BreakOutBox.Data
 
                 // k.Leerkrachten.ToList().Add(leerkrachten[0]);
 
-
-
                 var s = new Sessie("ABC", "Sessie1", "Een testSessie", groepen, box);
                 s.Klas = k;
 
-
-
-
-
-                #region Comments
-
+                #region Comments OLD
                 //var Toegangscodes = new List<Toegangscode>();
                 //for (int i = 0; i <= 7; i++)
                 //{
@@ -245,12 +219,10 @@ namespace BreakOutBox.Data
                 //var TBA = new List<Opdracht> {
                 //     new Opdracht(1,null,0),
                 // };
-
                 #endregion
 
-
-                 _dbContext.Sessies.Add(s);
-                 _dbContext.SaveChanges();
+                _dbContext.Sessies.Add(s);
+                _dbContext.SaveChanges();
             }
         }
     }
