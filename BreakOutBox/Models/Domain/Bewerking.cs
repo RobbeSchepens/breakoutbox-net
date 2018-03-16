502e226a315216b6fbd14ba5d0708197e21f201c
Daan Dedecker <daan.dedecker.y0420@student.hogent.be>
2018-03-17 00:27:59 +0100

    add bewerking class

---
+++ BreakOutBox/Models/Domain/Bewerking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BreakOutBox.Models.Domain
+
+namespace BreakOutBox.Models.Domain
+{
+    public class Bewerking
+    {
+        public BewerkingMogenlijkheden BewerkingMogenlijkheden { get; set; }
+        public double GetalBewerking { get; set; }
+        public Bewerking()
+        {
+            
+        }
+
+        public double geefUitkomstAntwoordMetGroepsBewerking()
+        {
+            return 0;
+          
+        }
+
+
+    }
+}
