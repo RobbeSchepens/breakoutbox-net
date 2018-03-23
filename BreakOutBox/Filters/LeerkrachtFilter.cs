7619f5b028ef62245c2bb717cd79b008afd71546
Robbe Schepens <robbe.schepens.y2870@student.hogent.be>
2018-03-23 08:47:34 +0100

    Add SessieFilter, LeerkrachtFilter

---
+++ BreakOutBox/Filters/LeerkrachtFilter.cs
@@ -0,0 +1,25 @@
+﻿using BreakOutBox.Models.Domain;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace BreakOutBox.Filters
+{
+    [AttributeUsageAttribute(AttributeTargets.All, AllowMultiple = false)]
+    public class LeerkrachtFilter : ActionFilterAttribute
+    {
+        private readonly ILeerkrachtRepository _leerkrachtRepository;
+
+        public LeerkrachtFilter(ILeerkrachtRepository leerkrachtRepository)
+        {
+            _leerkrachtRepository = leerkrachtRepository;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.ActionArguments["leerkracht"] =
+                context.HttpContext.User.Identity.IsAuthenticated ?
+                _leerkrachtRepository.GetByEmail(context.HttpContext.User.Identity.Name) : null;
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ BreakOutBox/Filters/SessieFilter.cs
@@ -0,0 +1,52 @@
+﻿using BreakOutBox.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using System;
+
+namespace BreakOutBox.Filters
+{
+    [AttributeUsageAttribute(AttributeTargets.All, AllowMultiple = false)]
+    public class SessieFilter : ActionFilterAttribute
+    {
+        private readonly ISessieRepository _sessieRepository;
+        private Sessie _sessie;
+
+        public SessieFilter(ISessieRepository sessieRepository)
+        {
+            _sessieRepository = sessieRepository;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            try
+            {
+                _sessie = _sessieRepository.GetBySessieCode(ReadSessieFromSession(context.HttpContext));
+            }
+            catch (NullReferenceException)
+            {
+                throw new Exception("Er is geen sessiecode in de Session varaiabele.");
+            }
+            context.ActionArguments["sessie"] = _sessie;
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            WriteSessieToSession(_sessie, context.HttpContext);
+            base.OnActionExecuted(context);
+        }
+
+        private string ReadSessieFromSession(HttpContext context)
+        {
+            string sessiecode = context.Session.GetString("sessie") == null ?
+                null : JsonConvert.DeserializeObject<string>(context.Session.GetString("sessie"));
+            return sessiecode;
+        }
+
+        private void WriteSessieToSession(Sessie sessie, HttpContext context)
+        {
+            context.Session.SetString("sessie", JsonConvert.SerializeObject(sessie.Code));
+        }
+    }
+}
