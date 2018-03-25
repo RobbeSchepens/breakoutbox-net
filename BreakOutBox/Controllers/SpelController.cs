3cd57d4e3b29c3103814a528b34ae197d90d1979
Robbe Schepens <robbeschepens@hotmail.com>
2018-03-25 19:52:59 +0200

    Re-add SpelSpelenViewModel implementation

--- BreakOutBox/Controllers/SpelController.cs
+++ BreakOutBox/Controllers/SpelController.cs
@@ -19,10 +19,12 @@ namespace BreakOutBox.Controllers
         public IActionResult SpelSpelen(Sessie sessie, Groep groep)
         {
             SpelSpelenViewModel ssvm = new SpelSpelenViewModel();
-            ssvm.ProgressieInPad = groep.Pad.getProgressie();
-            ssvm.Opdracht = groep.Pad.getCurrentOpdracht();
-            ssvm.ToegangscodeVolgendeOefening = groep.Pad.getNextOpdracht().Toegangscode.Code.ToString();
-            ssvm.GroepId = groep.GroepId;
+            ssvm = geefSsvmAangepastTerug(ssvm, 
+                groep.Pad.getCurrentOpdracht(), 
+                ssvm.JuistGeantwoordOpgave = ssvm.JuistGeantwoordOpgave, 
+                ssvm.JuistGeantwoordtoegangscode = ssvm.JuistGeantwoordtoegangscode, 
+                groep.Pad.getNextOpdracht().Toegangscode.Code.ToString(), 
+                groep.Pad.getProgressie());
             return View(ssvm);
         }
 
