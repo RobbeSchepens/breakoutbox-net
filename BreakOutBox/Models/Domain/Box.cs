12d834604e960cdc52bb19ae4e20faf8d42650d8
Robbe Schepens <robbeschepens@hotmail.com>
2018-03-31 18:25:43 +0200

    Remove cancel from actie popup, change NaamBox to Naam

--- BreakOutBox/Models/Domain/Box.cs
+++ BreakOutBox/Models/Domain/Box.cs
@@ -8,7 +8,7 @@ namespace BreakOutBox.Models.Domain
     public class Box
     {
         public int BoxId { get; set; }
-        public string NaamBox { get; set; }
+        public string Naam { get; set; }
         public string Omschrijving { get; set; }
         public ICollection<Actie> Acties { get; set; }
         public ICollection<Oefening> Oefeningen { get; set; }
@@ -20,7 +20,7 @@ namespace BreakOutBox.Models.Domain
 
         public Box(ICollection<Actie> acties, ICollection<Oefening> oefeningen, ICollection<Toegangscode> toegangscodes, string omschrijving, string naambox)
         {
-            NaamBox = naambox;
+            Naam = naambox;
             Omschrijving = omschrijving;
             Acties = acties;
             Oefeningen = oefeningen;
